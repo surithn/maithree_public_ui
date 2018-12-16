3623d93c9529ddccd0593c7f00eec0a6c8cdf20b
Seetharaman, Narayanan(naseetharaman) <naseetharaman@paypal.com>
2018-12-16 14:35:20 +0530

    Changes for task mapping UI
    Changes for task mapping UI

--- src/app/services/url-config.ts
+++ src/app/services/url-config.ts
@@ -9,10 +9,10 @@ export class UrlService {
 	urlList: any = {
 		baseUrl 			: "/server/api/v1",
 		baseImageUrl		: "/server",
-		//localBaseUrl 		: "http://localhost:5555/server/api/v1",
-		//localBaseImageUrl	: "http://localhost:5555/server",
-		// klocalBaseUrl 		: "http://10.239.68.124:5555/server/api/v1",
-		// klocalBaseImageUrl	: "http://10.239.68.124:5555/server",
+		localBaseUrl 		: "http://localhost:5555/server/api/v1",
+		localBaseImageUrl	: "http://localhost:5555/server",
+		klocalBaseUrl 		: "http://10.239.68.124:5555/server/api/v1",
+		klocalBaseImageUrl	: "http://10.239.68.124:5555/server",
 		jlocalBaseUrl 		: "http://10.239.62.242:5555/server/api/v1",
 		jlocalBaseImageUrl	: "http://10.239.62.242:5555/server",
 		devBaseUrl			: "",
@@ -20,11 +20,11 @@ export class UrlService {
 	}
 
 	getBaseUrl() {
-		return this.urlList['jlocalBaseUrl'];
+		return this.urlList['klocalBaseUrl'];
 	}
 
 	getBaseUrlForImages(){
-		return this.urlList['jlocalBaseImageUrl'];
+		return this.urlList['klocalBaseImageUrl'];
 	}
 
 
