98ee7ab4bfafbd0e8d8fee486f59b96beee8d40e
Seetharaman, Narayanan(naseetharaman) <naseetharaman@paypal.com>
2019-02-03 01:12:10 +0530

    Changes for report view of students
    Changes for report view of students

--- src/app/dashboard/report/report.component.ts
+++ src/app/dashboard/report/report.component.ts
@@ -5,7 +5,6 @@ import * as _ from "lodash";
 import { Angular5Csv } from 'angular5-csv/Angular5-csv';
 import * as moment from 'moment';
 import { AppService } from '../../services/app-services';
-import { truncate } from 'fs';
 
 
 @Component({
